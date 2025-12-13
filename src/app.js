96e7af1cf62c74adfc8febee756c0d283f020829
Abdul Kader <kader.bd.developer@gmail.com>
2025-12-13 21:10:13 +0600

    not much

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
-import { test } from "./test/test.js";
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+
+
+const app = express();
+
+
+app.use(cors({
+    origin: process.env.CORS,
+    credentials: true
+}))
+
+
+app.use(express.urlencoded({extended: true, limit: '16kb'}))
+app.use(express.json({limit: '16kb'}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+// import Routes
+
+
+
+
+// Routes
+
+
+
+
+export{app};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import dotenv from 'dotenv';
 import { ConnectDB } from './db/index.js';
+import { app } from './app.js';
 dotenv.config('./.env')
 ConnectDB()
+
+
+.then(()=>{
+
+    const Port = process.env.PORT || 9000;
+
+    app.listen(Port, ()=>{ console.log(`server is running on port : http://localhost:${Port}`) })
+})
+
+.catch((error)=>{
+    console.log('MongoDB connection Error from src > index.js ' , error )
+})
